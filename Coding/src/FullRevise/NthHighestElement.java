aeb9813979663b00ab506ae435de486b66959c18
prashant <pklj231@gmail.com>
2021-10-11 23:30:52 +0530

    New changelist

---
+++ Coding/src/FullRevise/NthHighestElement.java
@@ -0,0 +1,19 @@
+package FullRevise;
+
+public class NthHighestElement {
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 6, 8, 2, 3};
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("2nd highest " + arr[arr.length - 2]);
+
+    }
+}
